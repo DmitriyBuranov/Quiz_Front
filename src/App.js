045f765c68ddedcbc9342cf35150765886f25889
dburanov <dburanov.sup@yandex.ru>
2021-10-18 21:56:33 +0300

    add bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Button, Alert, Card, Form, Navbar, NavDropdown, Nav, Container} from 'react-bootstrap'
+
+//API where we going to get questions and save
+const API_URL = 'localhost:5000'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="secondary" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Quiz Platform</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#StartNewRound">New Round</Nav.Link>
+                <NavDropdown title="Menu" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Add question</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Last Results</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>        
     </div>
   );
 }
